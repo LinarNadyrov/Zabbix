5867b61a3c6192ec3462a0278e787729a02cbc05
LinarNadyrov <linar.nadyrov@itbegin.ru>
2020-03-11 16:09:57 +0300

    fixed script for check_api

--- lk/check_api.sh
+++ lk/check_api.sh
@@ -4,7 +4,10 @@ FILE=api.log
 DIR=/opt/zabbix
 
 touch $DIR/$FILE
-docker exec -t $DOCKER_NAME telnet api 8081 > $DIR/$FILE
 chmod 0666 $DIR/$FILE
 chown user:group $DIR/$FILE
+docker exec -t $DOCKER_NAME bash -c "(sleep 1;echo -en "exit\r";sleep 1;)|telnet api 8081" > $DIR/$FILE 
 exit 0
+
+
+
--- lk/deletePIDapi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-PID=`ps -eaf | grep 'NAME_PID' | grep -v grep | awk '{print $2}'`
-if [[ "" !=  "$PID" ]]; then
-  echo "killing $PID"
-  kill $PID
-fi
