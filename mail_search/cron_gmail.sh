a3a33f076e3aed4d80679705aa3c0bf9e920f159
LinarNadyrov <linar.nadyrov@itbegin.ru>
2020-03-16 17:05:16 +0300

    update scripts for monitoring

--- mail_search/cron_gmail.sh
+++ mail_search/cron_gmail.sh
@@ -2,6 +2,7 @@
 path_dir=/opt/scripts/zabbix/mail_search/
 path_log=log_of_gmail.txt
 cd $path_dir
+touch $path_log
 python3 ./search_mail_for_gmail.py > ./$path_log
 chown zabbix:root ./$path_log
 exit 0
--- mail_search/cron_yandex.sh
+++ mail_search/cron_yandex.sh
@@ -2,6 +2,7 @@
 path_dir=/opt/scripts/zabbix/mail_search/
 path_log=log_of_yandex.txt
 cd $path_dir
+touch $path_log
 python3 ./search_mail_for_yandex.py > ./$path_log
 chown zabbix:root ./$path_log
 exit 0
